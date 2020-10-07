984e4c908776c75ce3fd882cc403fa53b5f5ad65
marcosaraujo2020 <marcosaraujo2015.1@gmail.com>
2020-10-07 13:46:22 -0300

    Update

--- Livro_Pense_Python/Capitulo 9/exercicio_9.3.py
+++ Livro_Pense_Python/Capitulo 9/exercicio_9.3.py
@@ -3,21 +3,24 @@ def main():
     arquivo = open('words.txt')
     letras_proibidas = str(input('Digite uma sequencia de letras: '))
 
+    total_palavras = 0
     for linha in arquivo:
         palavra = linha.strip()
+        analisar = avoids(palavra, letras_proibidas)
+        if analisar == True:
+            print(palavra)
+            total_palavras += 1
 
+    print(f'Total de palavras sem as letras proibidas: {total_palavras}')
 
     arquivo.close()
 
 
-
 def avoids(palavras, letras):
-
-
-
-
-
-
-
+    for i in letras:
+        if i in palavras:
+            return False
+    
+    return True
 
 main()
