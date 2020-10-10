ca0a1c6fd2f243411c22f4693222827fc1c1f4c6
marcosaraujo2020 <marcosaraujo2015.1@gmail.com>
2020-10-10 19:12:19 -0300

    Update

--- Lista_Prof_Fabio/Algoritmos_Exercicio-05-VETORES_MATRIZES/fb_ex5_q6.py
+++ Lista_Prof_Fabio/Algoritmos_Exercicio-05-VETORES_MATRIZES/fb_ex5_q6.py
@@ -1,15 +1,17 @@
 def main():
-    
     numero_binario = str(input('Informe um número binário com 8 bits: '))
     vetor = []
     for i in numero_binario:
         vetor.append(int(i))
 
     hexadecimal = converter_hexadecimal(vetor)
-    print(f'O número Binário {numero_binario} corresponde ao número Hexadecimal {hexadecimal}')
+    decimal = converter_decimal(vetor)
 
-   # decimal = converter_decimal(vetor)
-   # print(f'O número Binário {numero_binario} corresponde ao número Decimal {decimal}')
+    print('=='*20)
+    print(f'O número Binário {numero_binario} equivale ao: \n' \
+        + f'Número Hexadecimal >> {hexadecimal}\n' \
+        + f'Número Decimal >>>>>> {decimal}')
+    print('=='*20)
 
 
 def converter_hexadecimal(lista):
@@ -41,10 +43,7 @@ def analisar_hexadecimal(valor):
     resultado = valor + 55
     return chr(resultado)
             
-         
 
-
-'''
 def converter_decimal(lista):
     soma = 0
     i = len(lista) - 1
@@ -53,24 +52,5 @@ def converter_decimal(lista):
         soma += valor
         i -= 1
     return soma
-'''
-
 
 main()
-
-'''
-if soma >=0 and soma < 10:
-                termo += str(soma)
-            if soma == 10:
-                termo += 'A'
-            if soma == 11:
-                termo += 'B'
-            if soma == 12:
-                termo += 'C'
-            if soma == 13:
-                termo += 'D'
-            if soma == 14:
-                termo += 'E'
-            if soma == 15:
-                termo += 'F'
-'''
